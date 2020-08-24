9dc1acddeefb109141ab5714652667fa9ed0ff02
renaud allard <43856164+rebald@users.noreply.github.com>
2020-08-24 14:37:23 +0200

    Add files via upload

---
+++ TFE_TEMP_LORA.X/mcc_generated_files/LoRaWAN/pin_manager_lora_addons.h
@@ -0,0 +1,71 @@
+/******************************************************************************
+  @Company:
+    Microchip Technology Inc.
+
+  @File Name:
+    pin_manager_lora_addons.h
+
+  @Summary:
+    This is the Pin Manager LoRa Addons header file which serves as an extension for Pin Manager
+    module files generated using MPLAB(c) Code Configurator
+
+  @Description:
+    This header file provides LoRa-specific defines for pin APIs for all used pins.
+    Copyright (c) 2013 - 2016 released Microchip Technology Inc.  All rights reserved.
+
+    Microchip licenses to you the right to use, modify, copy and distribute
+    Software only when embedded on a Microchip microcontroller or digital signal
+    controller that is integrated into your product or third party product
+    (pursuant to the sublicense terms in the accompanying license agreement).
+
+    You should refer to the license agreement accompanying this Software for
+    additional information regarding your rights and obligations.
+
+    SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+    EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
+    MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
+    IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER
+    CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR
+    OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+    INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
+    CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
+    SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+    (INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+ *************************************************************************
+ *                           pin_manager_lora_addons.h
+ *
+ * Pin Manager LoRa addons header file
+ *
+ ******************************************************************************/
+
+
+#ifndef PIN_MANAGER_LORA_IF_H
+#define PIN_MANAGER_LORA_IF_H
+
+#ifdef    __cplusplus
+extern "C" {
+#endif
+
+#include "../pin_manager.h"
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifdef    __cplusplus
+}
+#endif
+
+#endif    /* PIN_MANAGER_LORA_IF_H */
+
+/**
+ End of File
+*/
