2b1f871489b16ced30425c93fb18e215e2379405
Nathan <nathan.henry@idweaver.com>
2020-08-31 01:12:49 +0200

    feat: changed view of temp

--- TFE_WEB/tfe/src/Controller/TemperatureController.php
+++ TFE_WEB/tfe/src/Controller/TemperatureController.php
@@ -16,10 +16,28 @@ class TemperatureController extends AbstractController
         $temps = $this->getDoctrine()
           ->getRepository(Temperature::class)
           ->findAll();
+        
+        // Triage seulement pour les valeurs ajoutées à la main pour démo
+        usort($temps, array($this, "date_sort"));
+
+        $timeString = [];
+
+        foreach ($temps as $value) {
+            $date = $value->getCreatedAt()->format('Y-m-d H:i:s');
+            array_push($timeString, ['celsius' => $value->getCelsius(), "createdAt" => $date]);
+        }
 
         return $this->render('temperature/index.html.twig', [
             'controller_name' => 'TemperatureController',
-            'temps' => $temps
+            'temps' => $temps,
+            'tempString' => $timeString,
         ]);
+    }
+
+    function date_sort($a, $b) {
+        $aTime = $a->getCreatedAt();
+        $bTime = $b->getCreatedAt();
+        if ($aTime==$bTime) return 0;
+        return ($aTime<$bTime)?-1:1;
     }
 }
